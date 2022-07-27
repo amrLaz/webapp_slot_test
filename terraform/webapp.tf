c89b23a468ab44a26a517b828c4f1d310aae812d
Amr LAZRAQ <amr.lazraq@exakis-nelite.com>
2022-07-27 15:32:19 +0200

    first commit rectifiée

---
+++ terraform/webapp.tf
@@ -0,0 +1,65 @@
+
+resource "azurerm_service_plan" "splan" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku_name            = "P1v2"
+  os_type             = "Windows"
+}
+
+
+resource "azurerm_windows_web_app" "webapp" {
+  name                = "webapp-tf"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.splan.location
+  service_plan_id     = azurerm_service_plan.splan.id
+
+  site_config {
+    always_on                = true
+    ftps_state               = "Disabled"
+    remote_debugging_enabled = false
+    vnet_route_all_enabled   = true
+    application_stack {
+      current_stack = "node"
+      node_version  = "16-LTS"
+    }
+
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_app_service_source_control" "example" {
+  app_id   = azurerm_windows_web_app.webapp.id
+  repo_url = "https://github.com/Azure-Samples/python-docs-hello-world"
+  branch   = "master"
+}
+
+
+resource "azurerm_windows_web_app_slot" "webappslot" {
+  name           = "webapp-tf-slot"
+  app_service_id = azurerm_windows_web_app.webapp.id
+
+
+  site_config {
+    always_on                = true
+    ftps_state               = "Disabled"
+    remote_debugging_enabled = false
+    vnet_route_all_enabled   = true
+    application_stack {
+      current_stack = "node"
+      node_version  = "16-LTS"
+    }
+  }
+
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+resource "azurerm_app_service_source_control_slot" "example" {
+  slot_id  = azurerm_windows_web_app_slot.webappslot.id
+  repo_url = "https://github.com/Azure-Samples/python-docs-hello-world"
+  branch   = "master"
+}
