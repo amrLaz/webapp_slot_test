1165137a11c9745041a6913a72ed94d634dc1735
Amr LAZRAQ <amr.lazraq@exakis-nelite.com>
2022-07-30 10:18:34 +0200

    modify login block

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,7 +11,9 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+  client_id = "18625e5d-e15d-4c63-b145-96f09f3f5a65"
+  client_secret = "gwj8Q~sAzYoHcD~t6H5uTn9mQuWwL6D~ZLkYNcpY"
+  tenant_id = "4f490be5-b518-483a-99f6-90b580ae0cb5"
   features {}
-  
 }
 
