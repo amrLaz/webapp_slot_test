570ab444225b24651449a97a6f18076724eb7fd1
Amr LAZRAQ <amr.lazraq@exakis-nelite.com>
2022-08-01 15:50:38 +0200

    add var

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -17,10 +17,10 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  client_id = "18625e5d-e15d-4c63-b145-96f09f3f5a65"
-  client_secret = "gwj8Q~sAzYoHcD~t6H5uTn9mQuWwL6D~ZLkYNcpY"
-  tenant_id = "4f490be5-b518-483a-99f6-90b580ae0cb5"
-  subscription_id = "39c87760-f7b4-4479-ba4c-a0ed22bc35e2"
+  client_id = var.tf_client_id
+  client_secret = var.tf_client_secret
+  tenant_id = var.tf_tenant_id
+  subscription_id = var.tf_subscription_id
   features {}
 }
 
